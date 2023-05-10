48cf2f604332675dc4f1e6535e387cd99ff92c77
Mihir Soni <mihirsoni826@gmail.com>
2023-05-11 00:48:00 +0530

    Fixed MethodNotAllowedException handler

--- emergency-reporting-service/src/main/java/com/w3w/controller/ExceptionHandlerAdvice.java
+++ emergency-reporting-service/src/main/java/com/w3w/controller/ExceptionHandlerAdvice.java
@@ -6,16 +6,20 @@ import com.w3w.model.Invalid3waErrorMessage;
 import com.w3w.model.ThreeWordAddressSuggestions;
 import com.w3w.model.ErrorMessage;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpClientErrorException.MethodNotAllowed;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandlerAdvice {
+public class ExceptionHandlerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({BadRequestException.class, HttpClientErrorException.BadRequest.class})
     @ResponseBody
@@ -49,17 +53,6 @@ public class ExceptionHandlerAdvice {
                 .body(errorMessage);
     }
 
-    @ExceptionHandler(MethodNotAllowed.class)
-    public ResponseEntity<ErrorMessage> handleMethodNotAllowedException(MethodNotAllowed e) {
-        ErrorMessage errorMessage = new ErrorMessage();
-        errorMessage.setErrorCode(HttpStatus.METHOD_NOT_ALLOWED.toString());
-        errorMessage.setErrorMessage(e.getMessage());
-
-        return ResponseEntity
-                .status(HttpStatus.METHOD_NOT_ALLOWED)
-                .body(errorMessage);
-    }
-
     @ExceptionHandler(JsonParseException.class)
     public ResponseEntity<ErrorMessage> handleJsonParseException(JsonParseException e) {
         ErrorMessage errorMessage = new ErrorMessage();
@@ -81,5 +74,19 @@ public class ExceptionHandlerAdvice {
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(errorMessage);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorMessage errorMessage = new ErrorMessage();
+        errorMessage.setErrorCode(HttpStatus.METHOD_NOT_ALLOWED.toString());
+        errorMessage.setErrorMessage(ex.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(errorMessage);
+    }
+
+
+
 
 }
