9fd1dbe9fe925f5d620bfa73570b5b34b36a37fb
Mihir Soni <mihirsoni826@gmail.com>
2023-05-10 22:45:52 +0530

    Added utility Constants file

---
+++ emergency-reporting-service/src/main/java/com/w3w/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.w3w.utils;
+
+public class Constants {
+
+    public static final String API_KEY = "318QA8P4";
+    public static final String GREAT_BRITAIN_COUNTRY_CODE = "GB";
+    public static final String ENGLISH_LANGUAGE_CODE = "en";
+    public static final String WELSH_LANGUAGE_CODE = "cy";
+
+}
